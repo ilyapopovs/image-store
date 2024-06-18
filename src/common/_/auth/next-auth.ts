f79b2535d5afa548e63a042089ce14bda050ae58
ilyapopovs <ilya@popovs.dev>
2024-06-18 23:01:57 +0300

    feat: store session in DB instead of the cookie

--- src/common/_/auth/next-auth.ts
+++ src/common/_/auth/next-auth.ts
@@ -1,6 +1,22 @@
+import { db } from '@/database';
+import {
+  accounts,
+  authenticators,
+  sessions,
+  users,
+  verificationTokens,
+} from '@/database/schema';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+    authenticatorsTable: authenticators,
+  }),
   providers: [GitHub],
 });
