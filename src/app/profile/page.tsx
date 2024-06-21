065c4b758bb607918dd0495d6131963166b39af1
ilyapopovs <ilya@popovs.dev>
2024-06-21 17:34:56 +0300

    feat: use Card for profile cards

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,9 @@
 import { auth } from '@/common/_/auth/next-auth';
+import { Card, CardContent, CardHeader, CardTitle } from '@/common/_/ui/card';
 import { db } from '@/database';
 import { stripe_customers } from '@/database/schema/app.schema';
 import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
-import type { ReactNode } from 'react';
 
 export default async function Profile() {
   const session = await auth();
@@ -17,28 +17,27 @@ export default async function Profile() {
 
   return (
     <div className="p-8">
-      <div className="flex flex-wrap justify-center gap-8">
-        <ProfileCard>
-          <div>Name: {session?.user?.name}</div>
-          <div>Email: {session?.user?.email}</div>
-        </ProfileCard>
-        <ProfileCard>
-          <div>Stripe data:</div>
-          <div>
+      <div className="flex flex-wrap items-start justify-center gap-8">
+        <Card>
+          <CardHeader>
+            <CardTitle>Auth data</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div>Name: {session?.user?.name}</div>
+            <div>Email: {session?.user?.email}</div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Stripe data</CardTitle>
+          </CardHeader>
+          <CardContent>
             {stripeCustomer
               ? JSON.stringify(stripeCustomer, undefined, 2)
               : 'empty'}
-          </div>
-        </ProfileCard>
+          </CardContent>
+        </Card>
       </div>
-    </div>
-  );
-}
-
-function ProfileCard({ children }: { children: ReactNode }) {
-  return (
-    <div className="w-max rounded-md border bg-card p-4 text-card-foreground">
-      <div className="space-y-2 whitespace-pre-wrap">{children}</div>
     </div>
   );
 }
