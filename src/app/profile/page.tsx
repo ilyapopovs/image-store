bf2d3628fd7d7c8d65b0fc6dafd8c43d8c9f4a2e
ilyapopovs <ilya@popovs.dev>
2024-06-21 16:09:14 +0300

    feat: add stripe customer retrieval logic to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,7 @@
 import { auth } from '@/common/_/auth/next-auth';
+import { db } from '@/database';
+import { stripe_customers } from '@/database/schema/app.schema';
+import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 import type { ReactNode } from 'react';
 
@@ -9,6 +12,9 @@ export default async function Profile() {
     return redirect('/');
   }
 
+  const stripeCustomer = await getStripeCustomerData(session.user?.id!);
+  console.log('stripeCustomer', stripeCustomer);
+
   return (
     <div className="p-8">
       <div className="flex flex-wrap justify-center gap-8">
@@ -16,7 +22,14 @@ export default async function Profile() {
           <div>Name: {session?.user?.name}</div>
           <div>Email: {session?.user?.email}</div>
         </ProfileCard>
-        <ProfileCard>Stripe data: ...</ProfileCard>
+        <ProfileCard>
+          <div>Stripe data:</div>
+          <div>
+            {stripeCustomer
+              ? JSON.stringify(stripeCustomer, undefined, 2)
+              : 'empty'}
+          </div>
+        </ProfileCard>
       </div>
     </div>
   );
@@ -28,4 +41,17 @@ function ProfileCard({ children }: { children: ReactNode }) {
       <div className="space-y-2 whitespace-pre-wrap">{children}</div>
     </div>
   );
+}
+
+async function getStripeCustomerData(userId: string) {
+  const data = await db
+    .select()
+    .from(stripe_customers)
+    .where(eq(stripe_customers.user_id, userId));
+
+  if (data.length < 1) {
+    return undefined;
+  }
+
+  return data[0];
 }
