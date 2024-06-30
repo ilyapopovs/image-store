5ef71b3bd6f8cccc6d1a926c96785a10abcf1af0
ilyapopovs <ilya@popovs.dev>
2024-06-30 22:16:54 +0300

    feat: add my own custom-made dummy images instead of linking to external ones

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,21 +1,24 @@
+import photo1 from '@/common/assets/photo1.svg';
+import photo2 from '@/common/assets/photo2.svg';
+import photo3 from '@/common/assets/photo3.svg';
+import photo4 from '@/common/assets/photo4.svg';
+import photo5 from '@/common/assets/photo5.svg';
+import photo6 from '@/common/assets/photo6.svg';
+import Image from 'next/image';
 import { DownloadButton } from './_/download-button';
 
 export default function PhotosPage() {
-  const images = [
-    'https://images.pexels.com/photos/15080027/pexels-photo-15080027/free-photo-of-pile-of-firewood.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750',
-    'https://images.pexels.com/photos/5591708/pexels-photo-5591708.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750',
-    'https://images.pexels.com/photos/15525689/pexels-photo-15525689/free-photo-of-logs-in-moss.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750',
-  ];
+  const images = [photo1, photo2, photo3, photo4, photo5, photo6];
 
   return (
     <div className="flex flex-wrap justify-around gap-8 p-8">
       {images.map((image, index) => (
         <div key={index} className="flex flex-col items-center">
-          <div className="mb-2 aspect-video max-h-48 overflow-hidden rounded-lg">
-            <img
+          <div className="mb-2 aspect-video max-h-48 overflow-hidden rounded-lg bg-white">
+            <Image
               src={image}
               alt={`Image ${index + 1}`}
-              className="h-full w-full object-cover"
+              className="h-full w-full object-contain"
             />
           </div>
           <DownloadButton image={image} />
